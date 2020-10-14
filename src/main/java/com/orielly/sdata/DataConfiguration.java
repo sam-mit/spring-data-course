12cf30976d8d8af4f1ccb86922de6f9495899b62
soumits <soumit.saha@vodafone.com>
2020-10-14 22:42:34 +0100

    configuration

---
+++ src/main/java/com/orielly/sdata/DataConfiguration.java
@@ -0,0 +1,58 @@
+package com.orielly.sdata;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.EntityManagerFactoryUtils;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories("com.orielly.sdata")
+@EnableTransactionManagement
+@ComponentScan("com.orielly.sdata")
+public class DataConfiguration {
+
+  @Bean
+  public DataSource dataSource() {
+    EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+    return builder.setType(EmbeddedDatabaseType.H2).build();
+  }
+
+  // TODO review can this be done with £ntityManagerFactoryBuilder
+  @Bean("entityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    vendorAdapter.setGenerateDdl(true);
+
+    Properties jpaProperties = new Properties();
+    jpaProperties.put("hibernate.hbm2ddl.auto", "create-drop");
+    jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+
+    LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+    factoryBean.setDataSource(dataSource());
+    factoryBean.setPackagesToScan("com.orielly.sdata");
+    factoryBean.setJpaVendorAdapter(vendorAdapter);
+    factoryBean.setJpaProperties(jpaProperties);
+    factoryBean.afterPropertiesSet();
+    return factoryBean;
+  }
+
+  @Bean
+  public PlatformTransactionManager serviceGroupTransactionManager() {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+    return transactionManager;
+  }
+}
